a5f0fac7d52e1df2c08013c1cbd949070ff463af
jjflorezr <124735058+jjflorezr@users.noreply.github.com>
2023-03-06 23:41:57 -0500

    Corrección de datos en data.repaired

---
+++ Corrección de datos.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import os
+
+data = pd.read_csv("C:/Users/JUAN JOSE F ROMERO/Documents/UNIANDES/2023-01/Analítica Comp/Proyecto_1actd/processed.cleveland.data")
+print(data.head())
+
+data.replace("?", np.nan, inplace=True)
+
+# Convertimos todas las columnas a tipo float para poder calcular el promedio
+data = data.astype(float)
+
+# Calculamos el promedio de la columna "ca", excluyendo los valores faltantes
+ca_mean = data["ca"].mean(skipna=True)
+
+# Reemplazamos los valores faltantes en la columna "ca" con el promedio
+data["ca"].fillna(ca_mean, inplace=True)
+
+# Guardamos los datos en un nuevo archivo
+data.to_csv("processed.cleveland_repaired.csv", index=False, header=None)
