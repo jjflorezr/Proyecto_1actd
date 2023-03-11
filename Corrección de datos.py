17ae58af926094bcd3fb429960e0cd55226d5a0d
sofiabarrera <as.barrera@uniandes.edu.co>
2023-03-10 19:55:22 -0500

    Graficos

--- Corrección de datos.py
+++ Corrección de datos.py
@@ -4,7 +4,7 @@ import os
 
 a=""
 ejecutador= input("Quien esta corriendo?")
-if ejecutador=="Sofia":
+if ejecutador=="sofia":
     a="C:\\Users\\asbar\\OneDrive - Universidad de los Andes\\Carrera\\Séptimo semestre 2023-1\\ANALITICA\\Proyecto_1actd\\processed.cleveland.data"
 
 data = pd.read_csv(a)
@@ -25,4 +25,54 @@ data["ca"].fillna(ca_mean, inplace=True)
 # Guardamos los datos en un nuevo archivo
 data.to_csv("processed.cleveland_repaired.csv", index=False, header=None)
 
-print("y mi regalo?")
+#Graficas descriptivas
+
+import matplotlib.pyplot as plt
+
+# Variables
+edades= data['age'].tolist()
+sex=data['sex'].tolist()
+cp= data['cp'].tolist()
+trestbps= data['trestbps'].tolist()
+chol= data['chol'].tolist()
+fbs= data['fbs'].tolist()
+restecg= data['restecg'].tolist()
+thalach= data['thalach'].tolist()
+exang= data['exang'].tolist()
+oldpeak= data['oldpeak'].tolist()
+slope= data['slope'].tolist()
+ca= data['ca'].tolist()
+thal= data['thal'].tolist()
+num= data['num'].tolist()
+
+# Diagramas de caja para edades,trestbps,chol,thalach
+
+todos=[edades,trestbps,chol,thalach]
+plt.boxplot(todos)
+
+plt.title('Diagrama de Caja')
+plt.xlabel('Variables')
+plt.ylabel('Valores')
+
+nombres_variables = ['age', 'trestbp', 'chol','thalach']
+plt.xticks([1, 2, 3, 4], nombres_variables)
+plt.show()
+
+#Histograma de la edad
+
+plt.hist(edades, bins=10, color='#E69F00',edgecolor='black', alpha=0.7)
+plt.title('Distribución de la edad')
+plt.xlabel('Edades')
+plt.ylabel('Frecuencia')
+
+media = np.mean(edades)
+plt.axvline(media, color='red', linestyle='dashed', linewidth=1)
+plt.legend(['Media'], loc='upper right')
+plt.show()
+
+#Histograma del sexo
+plt.hist(sex, bins=1, color='#0000FF',edgecolor='black', alpha=0.7)
+plt.title('Distribución del sexo')
+plt.xlabel('Sexo')
+plt.ylabel('Frecuencia')
+plt.show()
